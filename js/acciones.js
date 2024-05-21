4357cb408cbf4279eb77c0d7077fc433c7cbf2d2
lightbblue <jstachioni@gmail.com>
2024-05-21 20:27:24 -0300

    Cambios 21/5

--- js/acciones.js
+++ js/acciones.js
@@ -1,1 +1,43 @@
-console.log("hola");
+let cantidad = document.getElementById("cantidad") 
+let btnrestar = document.getElementById("restar")
+let btnsumar = document.getElementById("sumar")
+let subtotal = document.getElementById("subtotal")
+let precioUnit = document.querySelector(".puntuResto")
+let unit = 1890
+precioUnit.innerText = `$${unit}`
+let cant = 1
+cantidad.innerText = cant
+subtotal.innerText = `Total: $${unit*cant}`
+btnrestar.innerText = "block"
+btnrestar.style.color = "grey"
+btnrestar.style.curso = "unset"
+function restar(){
+    if(cant>1){
+        cant = cant - 1
+        cantidad.innerText = cant
+        btnrestar.innerText = "remove"
+        btnrestar.style.color = "black"
+        btnsumar.innerText = "add"
+        btnsumar.style.color = "black"
+    }
+    if(cant===1){
+        btnrestar.innerText = "block"
+        btnrestar.style.color = "gray"
+    }
+    subtotal.innerText = `Total: $${unit*cant}`
+}
+function sumar(){
+    if(cant<4){
+        cant = cant + 1
+        cantidad.innerText = cant
+        btnsumar.innerText = "add"
+        btnsumar.style.color = "black"
+        btnrestar.innerText = "remove"
+        btnrestar.style.color = "black"
+    }
+    if(cant===4){
+        btnsumar.innerText = "block"
+        btnsumar.style.color = "gray"
+    }
+    subtotal.innerText = `Total: $${unit*cant}`
+}
